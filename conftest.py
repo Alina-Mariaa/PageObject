259442f29c826e7914b42d8b87495c606f210f44
levkovich <a.levkovich@complitech.org>
2019-07-11 11:47:39 +0300

    add assert for exception

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from selenium import webdriver
 
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language")
 
 @pytest.fixture(scope="function")
 def browser():
